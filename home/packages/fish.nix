a30277abbc87e6231dac8e661d1b9fe21a1b9250
sleepingoneiroi <sleepingoneiroi@proton.me>
2024-07-28 15:55:45 -0500

    fixed dots commands

--- home/packages/fish.nix
+++ home/packages/fish.nix
@@ -2,8 +2,8 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      dots-rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles#doremy";
-      dots-update = "sudo nixos-rebuild switch --upgrade --flake ~/dotfiles#doremy";
+      dots-rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles#doremy";
+      dots-update = "sudo nixos-rebuild switch --upgrade --flake ~/.dotfiles#doremy";
     };
   };
 }

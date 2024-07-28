388f8c07bad483b93c30c3bd130bc6704fc4f829
sleepingoneiroi <sleepingoneiroi@proton.me>
2024-07-28 00:03:31 -0500

    added sagume to flake

--- flake.nix
+++ flake.nix
@@ -18,7 +18,6 @@
 
   outputs = {self, nixpkgs, home-manager, catppuccin, nix-colors, catppuccin-vsc, ...} @inputs: {
     nixosConfigurations = {
-      
       doremy = nixpkgs.lib.nixosSystem{
         system = "x86_64-linux";
         
@@ -27,6 +26,26 @@
         };
         modules = [
           ./hosts/doremy
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              users.oneiroi = import ./home;
+              extraSpecialArgs = {inherit inputs;};
+            };
+          }
+          catppuccin.nixosModules.catppuccin
+        ];
+      };
+      sagume = nixpkgs.lib.nixosSystem{
+        system = "x86_64-linux";
+        
+        specialArgs = { 
+          inherit inputs;
+        };
+        modules = [
+          ./hosts/sagume
 
           home-manager.nixosModules.home-manager
           {
